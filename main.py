7a7ce54ca97efc08674925db40821c9342c08a44
Ahmed Alhisan <alhisan.swe@gmail.com>
2023-01-18 11:43:44 +0300

    edit solv

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
-
-
 def additoin(x, y):
-    try:
-        x = 10
-        y = 20
-        print("Addition:", x + b)
-    except NameError :
-        print("you try to use a variable dose not exist ")
-    except Exception as error:
-        print("Somthing Wrong !! :  " , error.__class__)
-    else:
-        print("the operation is successful")     
-additoin(10, 20)           
+          x = 10
+          y = 20
+          print("Addition:", x + b)
 
 
-
-
+try:
+    additoin(10, 20)
+except NameError:
+    print("you try to use a variable dose not exist" )
+except Exception as error:
+    print("error" , error.__class__)    
+else:
+    print("the operation is successful")    
