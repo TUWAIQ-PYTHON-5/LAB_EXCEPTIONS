81bb8d7309a3ed3fa53d6e0b28ba620b8dac24af
Dooha005 <MINGW64@gmail.com~>
2023-01-18 11:25:35 +0300

    SOLVED LAB ..

---
+++ LAB_EX.py
@@ -0,0 +1,21 @@
+try :
+
+    def additoin(x, y):
+          x = 10
+          y = 20
+          print("Addition:", x + y)
+
+
+    additoin(10, 20)
+
+    print ("the operation is successful")
+
+except NameError :
+
+    print (" There are a error in Name ")
+
+except :
+    
+    print (" There are a error ")
+  
+
