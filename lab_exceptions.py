65164c57de99bb105b4718807977baaab610b492
Anfal Ali <name@gmail.com>
2023-01-18 11:32:10 +0300

    LAB_EXCEPTIONS

---
+++ lab_exceptions.py
@@ -0,0 +1,17 @@
+ 
+try:
+    def additoin(x, y):
+          x = 10
+          y = 20
+          print("Addition:", x + b)
+
+
+    additoin(10, 20)
+except NameError as error:
+     print("Name b is not defined",error.__class__)
+except IndentationError as error:
+     print("Unexpected indent",error.__class__)
+else:
+      print("Operation needed to be done if successful")
+
+     
