5003f37f44178edffc35c573c897f572611dfe0b
محمد الزيداني <zidani2030@gmail.com>
2023-01-18 11:42:10 +0300

    solved

---
+++ lab.py
@@ -0,0 +1,20 @@
+
+def additoin(x, y):
+        x = 10
+        y = 20
+    
+        print("Addition:", x + b)
+
+
+try:
+    additoin(10, 20)
+
+#except Exception as error:
+ #   print("There was an error", error.__class__)
+
+except NameError:
+
+    print("Error defining variables")
+
+else:
+    print( "the operation is successful")
