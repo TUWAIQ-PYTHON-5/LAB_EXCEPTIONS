d71229dd52e73c31e1e3a50d70a700e27cd2b34f
atheer <atheeralq0@gmail.com>
2023-01-18 12:10:42 +0300

    end lab10

---
+++ lab10.py
@@ -0,0 +1,16 @@
+
+def additoin(x, y):
+    x = 10
+    y = 20
+    print("Addition:", x + b)
+
+
+
+try:
+     additoin(10, 20)
+except NameError:
+    print("the error is in Nameerror try again")
+except:
+    print("thar is in error")
+else:
+    print("the operation is successful")
