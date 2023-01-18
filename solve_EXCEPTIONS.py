825f52030588eacefa8833c2c7d1100361b50f8e
asrar <asrar.rifae@hotmail.com>
2023-01-18 11:23:51 +0300

    solve LAB_EXCEPTIONS lab

---
+++ solve_EXCEPTIONS.py
@@ -0,0 +1,15 @@
+
+try :  
+      def additoin(x, y):
+          x = 10
+          y = 20
+          print("Addition:", x + b)
+      additoin(10, 20)
+except IndentationError:
+    print("please check the Improper indentation")
+except NameError: 
+        print("please check variables again ")
+except :
+    print("something went wrong , please try again !! ")
+else :
+    print("the operation is successful")
