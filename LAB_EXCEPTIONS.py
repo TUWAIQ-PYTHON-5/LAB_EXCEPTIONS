b68647b270a3ec2db51f1c6788c813a3f990affa
NjoudAlhazmi <njoudz19@gmail.com>
2023-01-18 11:28:37 +0300

    solve it

---
+++ LAB_EXCEPTIONS.py
@@ -0,0 +1,15 @@
+try:
+
+ def additoin(x, y):
+    x = 10
+    y = 20
+    print("Addition:", x + b)
+    additoin(10, 20)
+
+
+except NameError:
+    print("Please check variables")
+except :
+    print(" please try again): ")
+else:
+    print("the operation is successful")
