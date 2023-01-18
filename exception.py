95549df1c377e2b9ed4665853d4a2dea52b1c292
Sara Alotaibi <alotaibisara49@gmail.com>
2023-01-18 11:35:49 +0300

    solved lab

---
+++ exception.py
@@ -0,0 +1,19 @@
+
+try:
+    def additoin(x, y):
+       x = 10
+       y = 20
+       print("Addition:", x + b)
+    additoin(10, 20)
+
+except NameError :
+    print("plese enter the name b: ")
+    
+except :
+    print ("There was an error" )
+
+else:
+    print("the operation is successful")
+
+
+
