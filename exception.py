72ea59dabf1712b8c17097a4d74455ce16fc19a8
Faimubarak <102253601+Faimubarak@users.noreply.github.com>
2023-01-18 11:48:10 +0300

    done

---
+++ exception.py
@@ -0,0 +1,14 @@
+def additoin(x, y):  
+          x = 10
+          y = 20
+          print("Addition:", x + y)
+
+try:
+      additoin(10, 20)
+except NameError:
+    print(" b is not define..")
+
+except:
+    print("Operation needed to be done if successful")
+else:
+    print("the operation is successful")
