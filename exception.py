b8c9f0e943dd8e596accf9bd86cfc7c3464b83e2
ZeNullOrb <elyas1999sb@hotmail.com>
2023-01-18 11:21:25 +0300

    exception lab

---
+++ exception.py
@@ -0,0 +1,11 @@
+def additoin(x, y):
+    x = 10
+    y = 20
+    try:
+        print("Addition:", x + b)
+    except NameError:
+        print("the name of the variable is wrong")
+    else:
+        print("the operation is successful")
+
+additoin(10, 20)
